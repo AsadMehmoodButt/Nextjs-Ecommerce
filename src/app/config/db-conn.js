66a489e21483cbb82d6a0b4be40a8fd21c58b65c
AsadMehmoodButt <asad.mehmood@arhamsoft.org>
2024-04-27 14:33:29 +0500

    initial commit

--- src/app/config/db-conn.js
+++ src/app/config/db-conn.js
@@ -3,7 +3,7 @@ const mongose = require("mongoose");
 const Connectdbs = async () => {
   mongose.set("strictQuery", false);
   try {
-    await mongose.connect("mongodb+srv://asad:Asad1234$@cluster0.esgjxuk.mongodb.net/");
+    await mongose.connect(process.env.MONGO_URI);
     console.log("connected successfuly");
   } catch (error) {
     console.log("Could not connect DB");
