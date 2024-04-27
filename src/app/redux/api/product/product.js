29070475d8ae7ef1d66660d69e4c0d15886c7f82
AsadMehmoodButt <asad.mehmood@arhamsoft.org>
2024-04-27 15:43:33 +0500

    initial commit

--- src/app/redux/api/product/product.js
+++ src/app/redux/api/product/product.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const Product = createApi({
   reducerPath: "Product",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:3000/",
+    baseUrl: process.env.NEXTAUTH_UTL,
   }),
 
   tagTypes: ["product"],
