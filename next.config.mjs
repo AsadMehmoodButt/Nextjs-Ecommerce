30ee99bd64ed08427924ffae87cdcda03877367e
AsadMehmoodButt <asad.mehmood@arhamsoft.org>
2024-04-27 14:05:55 +0500

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    DB_URI: "mongodb://localhost:27017/ecommNext",
+    DB_URI: "mongodb+srv://asad:Asad1234$@cluster0.esgjxuk.mongodb.net/",
   },
 };
 
--- src/app/config/db-conn.js
+++ src/app/config/db-conn.js
@@ -3,7 +3,7 @@ const mongose = require("mongoose");
 const Connectdbs = async () => {
   mongose.set("strictQuery", false);
   try {
-    await mongose.connect("mongodb://localhost:27017/ecommNext");
+    await mongose.connect("mongodb+srv://asad:Asad1234$@cluster0.esgjxuk.mongodb.net/");
     console.log("connected successfuly");
   } catch (error) {
     console.log("Could not connect DB");
