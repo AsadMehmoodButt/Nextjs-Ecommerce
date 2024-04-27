024cedc301c1869c1cfcafb3a583c4b173fc1de3
AsadMehmoodButt <asad.mehmood@arhamsoft.org>
2024-04-27 14:58:05 +0500

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    DB_URI: "mongodb+srv://asad:Asad1234$@cluster0.esgjxuk.mongodb.net/",
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
